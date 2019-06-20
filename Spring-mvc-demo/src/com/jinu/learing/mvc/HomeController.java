7e1402c0e5bfb0431494dca16134ba7c434392b8
cluebox <jinucluebox@gmail.com>
2019-06-20 13:40:50 +0530

    Final commit

--- Spring-mvc-demo/src/com/jinu/learing/mvc/HomeController.java
+++ Spring-mvc-demo/src/com/jinu/learing/mvc/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 	@RequestMapping("/")
-	public String getHomepage(){
+	public String getHomepage(){   
 		return "main-menu";
 	}
 
